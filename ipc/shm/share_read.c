02e479428e0a7175ae0554e5735d391b4446cbd9
yang1127 <1536520643@qq.com>
2020-03-28 21:58:59 +0800

    共享内存练习

---
+++ ipc/shm/share_read.c
@@ -0,0 +1,36 @@
+//共享内存
+//int shmget(key_t key, size_t size, int shmflg);
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#define IPC_KEY 0x12345678 //ftok:key_t ftok
+#define SHM_SIZE 4096
+
+int main()
+{
+	int shmid = shmget(IPC_KEY, SHM_SIZE, IPC_CREAT | 0664);
+	if (shmid < 0)
+	{
+		perror("shmget error\n");
+	} 
+	
+	//void *shmat(int shmid, const void *shmaddr, int shmflg);
+	void *shm_start = shmat(shmid, NULL, 0); //任何方式
+	if (shm_start == (void*) - 1)
+	{
+		perror("shmat error\n");
+	}
+	int i = 0;
+	while(1)
+	{
+		printf("%s",(char*) shm_start);	
+		sleep(1);
+	}
+	shmdt(shm_start);
+	shmctl(shmid, IPC_RMID, NULL);	
+	return 0;
+}
